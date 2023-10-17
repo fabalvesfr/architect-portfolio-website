6b7802d435c3165ae8a942aa585ada551481351e
fabalvesfr <fabio9162@gmail.com>
2023-10-17 18:12:18 +0200

    homepage ok, login ok, now working on edition mode

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,153 +1,148 @@
-
-const apiURL = 'http://localhost:5678/api/';
+const apiURL = "http://localhost:5678/api/";
 
 // *********** FUNCTIONS ***************
 
 // Fetching works from API
-async function getWorks(){
-    let response = await fetch(apiURL+'works');
-    let works = await response.json();
-    return works;
+async function getWorks() {
+  let response = await fetch(apiURL + "works");
+  let works = await response.json();
+  return works;
 }
 
 // Display ALL works
 async function displayWorks() {
-    try {
-        const works = await getWorks();
+  try {
+    const works = await getWorks();
 
-        const gallery = document.getElementById('gallery');
+    const gallery = document.getElementById("gallery");
 
-        works.forEach(work => {
-            // Creating the DOM elements
-            let newFigure = document.createElement('figure');
-            let newImg = document.createElement('img');
-            let newCaption = document.createElement('figcaption');
+    works.forEach((work) => {
+      // Creating the DOM elements
+      let newFigure = document.createElement("figure");
+      let newImg = document.createElement("img");
+      let newCaption = document.createElement("figcaption");
 
-            // Setting DOM elements' attributes
-            newImg.setAttribute('src', work.imageUrl);
-            newImg.setAttribute('alt', work.title);
+      // Setting DOM elements' attributes
+      newImg.setAttribute("src", work.imageUrl);
+      newImg.setAttribute("alt", work.title);
 
-            newCaption.textContent = work.title;
+      newCaption.textContent = work.title;
 
-            // Appending DOM elements to their respective parent    
-            newFigure.appendChild(newImg);
-            newFigure.appendChild(newCaption);
-            gallery.appendChild(newFigure);
-        });
-
-    } catch (error) {
-        console.error("Error: ", error);
-    }
+      // Appending DOM elements to their respective parent
+      newFigure.appendChild(newImg);
+      newFigure.appendChild(newCaption);
+      gallery.appendChild(newFigure);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 }
 
 async function displayObjets() {
-    try {
-        const works = await getWorks();
+  try {
+    const works = await getWorks();
 
-        // Filtering works that belong to the 'Objets' category
-        let onlyObjetsWorks = works.filter(work => {
-            return work.category.name.toLowerCase() === 'objets';
-        }); 
-        
-        // Erasing previously displayed works on the DOM before displaying the newly filtered ones
-        const gallery = document.getElementById('gallery');
-        gallery.innerHTML = '';
+    // Filtering works that belong to the 'Objets' category
+    let onlyObjetsWorks = works.filter((work) => {
+      return work.category.name.toLowerCase() === "objets";
+    });
 
-        onlyObjetsWorks.forEach(work => {
-            // Creating the DOM elements
-            let newFigure = document.createElement('figure');
-            let newImg = document.createElement('img');
-            let newCaption = document.createElement('figcaption');
+    // Erasing previously displayed works on the DOM before displaying the newly filtered ones
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
 
-            // Setting DOM elements' attributes
-            newImg.setAttribute('src', work.imageUrl);
-            newImg.setAttribute('alt', work.title);
+    onlyObjetsWorks.forEach((work) => {
+      // Creating the DOM elements
+      let newFigure = document.createElement("figure");
+      let newImg = document.createElement("img");
+      let newCaption = document.createElement("figcaption");
 
-            newCaption.textContent = work.title;
+      // Setting DOM elements' attributes
+      newImg.setAttribute("src", work.imageUrl);
+      newImg.setAttribute("alt", work.title);
 
-            // Appending DOM elements to their respective parent    
-            newFigure.appendChild(newImg);
-            newFigure.appendChild(newCaption);
-            gallery.appendChild(newFigure);
-        });
+      newCaption.textContent = work.title;
 
-    } catch (error) {
-        console.error("Error: ", error);
-    }
+      // Appending DOM elements to their respective parent
+      newFigure.appendChild(newImg);
+      newFigure.appendChild(newCaption);
+      gallery.appendChild(newFigure);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 }
 
 async function displayAppartements() {
-    try {
-        const works = await getWorks();
+  try {
+    const works = await getWorks();
 
-        // Filtering works that belong to the 'Objets' category
-        let onlyAppartWorks = works.filter(work => {
-            return work.category.name.toLowerCase() === 'appartements';
-        }); 
-        
-        const gallery = document.getElementById('gallery');
-        gallery.innerHTML = '';
+    // Filtering works that belong to the 'Objets' category
+    let onlyAppartWorks = works.filter((work) => {
+      return work.category.name.toLowerCase() === "appartements";
+    });
 
-        // Displaying each filtered work on the DOM
-        onlyAppartWorks.forEach(work => {
-            // Creating the DOM elements
-            let newFigure = document.createElement('figure');
-            let newImg = document.createElement('img');
-            let newCaption = document.createElement('figcaption');
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
 
-            // Setting DOM elements' attributes
-            newImg.setAttribute('src', work.imageUrl);
-            newImg.setAttribute('alt', work.title);
+    // Displaying each filtered work on the DOM
+    onlyAppartWorks.forEach((work) => {
+      // Creating the DOM elements
+      let newFigure = document.createElement("figure");
+      let newImg = document.createElement("img");
+      let newCaption = document.createElement("figcaption");
 
-            newCaption.textContent = work.title;
+      // Setting DOM elements' attributes
+      newImg.setAttribute("src", work.imageUrl);
+      newImg.setAttribute("alt", work.title);
 
-            // Appending DOM elements to their respective parent
+      newCaption.textContent = work.title;
 
-            newFigure.appendChild(newImg);
-            newFigure.appendChild(newCaption);
-            gallery.appendChild(newFigure);
-        });
+      // Appending DOM elements to their respective parent
 
-    } catch (error) {
-        console.error("Error: ", error);
-    }
+      newFigure.appendChild(newImg);
+      newFigure.appendChild(newCaption);
+      gallery.appendChild(newFigure);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 }
 
 async function displayhotelsResto() {
-    try {
-        const works = await getWorks();
+  try {
+    const works = await getWorks();
 
-        // Filtering works that belong to the 'Objets' category
-        let onlyHotelsRestoWorks = works.filter(work => {
-            return work.category.name.toLowerCase() === 'hotels & restaurants';
-        }); 
-        
-        const gallery = document.getElementById('gallery');
-        gallery.innerHTML = '';
+    // Filtering works that belong to the 'Objets' category
+    let onlyHotelsRestoWorks = works.filter((work) => {
+      return work.category.name.toLowerCase() === "hotels & restaurants";
+    });
 
-        // Displaying each filtered work on the DOM
-        onlyHotelsRestoWorks.forEach(work => {
-            // Creating the DOM elements
-            let newFigure = document.createElement('figure');
-            let newImg = document.createElement('img');
-            let newCaption = document.createElement('figcaption');
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
 
-            // Setting DOM elements' attributes
-            newImg.setAttribute('src', work.imageUrl);
-            newImg.setAttribute('alt', work.title);
+    // Displaying each filtered work on the DOM
+    onlyHotelsRestoWorks.forEach((work) => {
+      // Creating the DOM elements
+      let newFigure = document.createElement("figure");
+      let newImg = document.createElement("img");
+      let newCaption = document.createElement("figcaption");
 
-            newCaption.textContent = work.title;
+      // Setting DOM elements' attributes
+      newImg.setAttribute("src", work.imageUrl);
+      newImg.setAttribute("alt", work.title);
 
-            // Appending DOM elements to their respective parent
+      newCaption.textContent = work.title;
 
-            newFigure.appendChild(newImg);
-            newFigure.appendChild(newCaption);
-            gallery.appendChild(newFigure);
-        });
+      // Appending DOM elements to their respective parent
 
-    } catch (error) {
-        console.error("Error: ", error);
-    }
+      newFigure.appendChild(newImg);
+      newFigure.appendChild(newCaption);
+      gallery.appendChild(newFigure);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 }
 
 // *******************************************
@@ -155,34 +150,51 @@ async function displayhotelsResto() {
 // *********** EVENT LISTENERS ***************
 
 // Displaying ALL works upon page load
-document.addEventListener('DOMContentLoaded', ()=>{
-    displayWorks();
+document.addEventListener("DOMContentLoaded", () => {
+  displayWorks();
 });
 
 // Displaying all works upon click on "Tous" button
-const tous = document.getElementById('tous');
-tous.addEventListener('click', () => {
-    displayWorks();
+const tous = document.getElementById("tous");
+tous.addEventListener("click", () => {
+  displayWorks();
 });
 
-
 // Filtering Object  works
-const objets = document.getElementById('objets');
-objets.addEventListener('click', () => {    
-    displayObjets();
+const objets = document.getElementById("objets");
+objets.addEventListener("click", () => {
+  displayObjets();
 });
 
 // Filtering Appartment works
-const appartements = document.getElementById('appartements');
-appartements.addEventListener('click', () => {    
-    displayAppartements();
+const appartements = document.getElementById("appartements");
+appartements.addEventListener("click", () => {
+  displayAppartements();
 });
 
 // Filtering Hotels and Restaurants works
-const hotelsResto = document.getElementById('hotels-resto');
-hotelsResto.addEventListener('click', ()=>{
-    displayhotelsResto();
+const hotelsResto = document.getElementById("hotels-resto");
+hotelsResto.addEventListener("click", () => {
+  displayhotelsResto();
 });
 
 // *******************************************
 
+// Retrieving the token variable from local storage
+const token = localStorage.getItem("token");
+
+if (token) {
+  // Enabling "Edition mode" DOM elements
+  const bandeauEdition = document.querySelector("#bandeau-edition");
+  bandeauEdition.toggleAttribute("style");
+
+  const titreEdition = document.querySelector("#titre-edition");
+  titreEdition.toggleAttribute("style");
+}
+
+// Modal activation after clicking on the "Edit" icon
+document.querySelector("#icone-edition").addEventListener("click", () => {
+  const firstModal = document.querySelector("#first-modal");
+  firstModal.showModal();
+  // TODO: firstModal.close() when we click outside the modal
+});
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,43 +1,45 @@
 // ******** USER AUTHENTIFICATION ************
-loginUrl = 'http://localhost:5678/api/users/login';
+loginUrl = "http://localhost:5678/api/users/login";
 
-const loginForm = document.getElementById('login-form');
+const loginForm = document.getElementById("login-form");
 
-loginForm.addEventListener('submit', ($event) => {
-    $event.preventDefault(); // Empêche le chargement de la page par défaut
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+loginForm.addEventListener("submit", ($event) => {
+  $event.preventDefault(); // Empêche le chargement de la page par défaut
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    const userData = {
-        "email": email,
-        "password": password
+  const userData = {
+    email: email,
+    password: password,
+  };
+
+  async function login() {
+    const response = await fetch(loginUrl, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    // For '400 - Bad Request', '401 - Unauthorized', '500 - Unexpected Error'
+    // TODO: use Font Awesome and Bootstrap to creat a nice alert message
+    if (response.status != 201 && response.status != 200) {
+      userInvalid = document.createElement("small");
+      userInvalid.textContent = "Usager et/ou mot-de-passe incorrects";
+      loginForm.appendChild(userInvalid);
+    } else {
+      // Storing the login token on the brower after sucessful '201 - Created'
+      const data = await response.json();
+      const token = data.token;
+      // Using the local storage on the browse to storage a value called 'token' with the token retrieved via the POST request
+      localStorage.setItem("token", token);
+      console.log(token);
+      // Redirecting the browser to homepage after successfull login
+      window.location.href = "/FrontEnd/index.html";
     }
+  }
 
-    async function login(){
-        const response = await fetch(loginUrl, {
-            method: 'POST',
-            headers: {
-                'accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        });
-
-        // For '400 - Bad Request', '401 - Unauthorized', '500 - Unexpected Error'
-        if (response.status != 201){
-            userInvalid = document.createElement('small');
-            userInvalid.textContent = 'Usager et/ou mot-de-passe incorrects';
-            loginForm.appendChild(userInvalid);
-        }
-        // For sucessful '201 - Created'
-        const data = await response.json();
-        console.log(data);
-
-        // TODO: stocker le token dans un local storage (stockage au niveau du navigateur)
-        // Rediriger vers la page d'accueil avec les icônes pour éditer si couple usager/mdp bon (vérifier d'abord si effectivement on a un toker de retourné)
-
-        // return data.token;
-    }
-
-    login();
+  login();
 });
